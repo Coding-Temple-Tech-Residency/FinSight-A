1e8f57cd715be20ad0ebedc75fd3ed25a3ca3a41
Nicholas Eaton <nchlseaton3@gmail.com>
2026-06-29 10:27:15 -0400

    feat: configure Redux store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,27 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { AuthProvider } from './context/AuthContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { AuthProvider } from "./context/AuthContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{redirect_uri: window.location.origin,}}
+    <Provider store={store}>
+      <Auth0Provider
+       domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
       >
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Auth0Provider>
+        <AuthProvider>
+         <App />
+       </AuthProvider>
+      </Auth0Provider>
+   </Provider>
   </StrictMode>,
-)
+);
