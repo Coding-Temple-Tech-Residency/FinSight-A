57e3cb4e35a6b3379e837d05b7b7f8a2fed73071
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-07-08 12:10:53 -0400

    fixed Navbar issues: login wasn't responding, formatting wasn't proper

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     const { isAuthenticated, user } = useAuth0();
     return (
         <nav className='bg-gray-900 text-white px-6 py-4'>
-            <div className='max-w-7xl mx-auto flext items-center justify-between'>
+            <div className='max-w-7xl mx-auto flex items-center justify-between'>
                 <div className='text-xl font-bold'>
                     FinSight
                 </div>
@@ -34,3 +34,56 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
+// function Navbar() {
+//     const { isAuthenticated, isLoading, user } = useAuth0();
+
+//     console.log({
+//         isLoading,
+//         isAuthenticated,
+//         user,
+//     })
+
+//     return (
+//         <nav className='bg-slate-900 text-white px-6 py-4'>
+//             <div className='max-w-7xl mx-auto flex items-center justify-between'>
+                
+//                 {/*Logo */}
+//                 <div className='text-xl font-bold'>
+//                     FinSight
+//                 </div>
+
+//                 {/*Links */}
+//                 <div className='flex gap-6'>
+//                     <a href="/dashboard" className="hover:text-blue-400 transition">
+//                     Dashboard
+//                     </a>
+                    
+//                     <a href="/watchlist" className="hover:text-blue-400 transition">
+//                     Watchlist
+//                     </a>
+
+//                     <a href="/portfolio" className="hover:text-blue-400 transition">
+//                     Portfolio
+//                     </a>
+
+//                     <a href="/profile" className="hover:text-blue-400 transition">
+//                     Profile
+//                     </a>
+                    
+//                 </div>
+//                 <div>
+//                     {isAuthenticated ? (
+//                     <LogoutButton />
+//                     ): (<LoginButton />)}
+                    
+//                 </div>
+                
+//             </div>
+//         </nav>
+//     )
+// }
+
+// export default Navbar;
